6ecc0b420e6b831dd95d17e5578fac0b8992a113
신혜원 <hwkong7_@naver.com>
2025-12-04 12:02:37 +0900

    Initial Commit

---
+++ Tint_Song/Tint_SongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tint_SongApp.swift
+//  Tint_Song
+//
+//  Created by student on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Tint_SongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tint_SongTests/Tint_SongTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tint_SongTests.swift
+//  Tint_SongTests
+//
+//  Created by student on 12/4/25.
+//
+
+import Testing
+@testable import Tint_Song
+
+struct Tint_SongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

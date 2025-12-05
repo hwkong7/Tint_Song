dbddd320c9bb9d8db71b8170999bbd0f18f80ec4
신혜원 <hwkong7_@naver.com>
2025-12-05 13:40:19 +0900

    주석 수정

--- Tint_Song/Extensions/Color+Hex.swift
+++ Tint_Song/Extensions/Color+Hex.swift
@@ -33,23 +33,3 @@ extension Color {
         self.init(red: red, green: green, blue: blue)
     }
 }
-
-//import SwiftUI
-//
-//extension Color {
-//    init?(hex: String) {
-//        var hexFormatted = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-//        hexFormatted = hexFormatted.replacingOccurrences(of: "#", with: "")
-//
-//        var rgb: UInt64 = 0
-//        guard Scanner(string: hexFormatted).scanHexInt64(&rgb) else {
-//            return nil
-//        }
-//
-//        let r = Double((rgb >> 16) & 0xFF) / 255
-//        let g = Double((rgb >> 8) & 0xFF) / 255
-//        let b = Double(rgb & 0xFF) / 255
-//
-//        self.init(red: r, green: g, blue: b)
-//    }
-//}
